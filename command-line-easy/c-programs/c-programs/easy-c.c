a4e2e4ae7d76e6330f35b50f81ab16d45506110c
zayheard <realkallofduty.100@gmail.com>
2026-09-04 19:58:13 +0000

    Completed easy C programming tasks

---
+++ command-line-easy/c-programs/c-programs/easy-c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num = 5;
+    int age;
+
+    printf("Number: %d\n", num);
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    printf("Age: %d\n", age);
+
+    return 0;
+}
+
